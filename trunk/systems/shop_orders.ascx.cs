96ee7ed600c21483ddef6d87167452dc4b8446e4
vsenih <vsenih@f233bf74-7c54-0410-ac8d-d1e801669cec>
2008-08-26 15:48:30 +0000

    26.08.2008 Notification API implementation finished, added new field in database "charged_amount". Starting with Orders API implementation

---
+++ trunk/systems/shop_orders.ascx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using Orders;
+
+public partial class systems_shop_orders : BaseUserControl
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        GridView ordersGrid = (GridView)LoginView.FindControl("OrdersGridView");
+        ordersGrid.DataSource = Orders.Orders.GetAllOrders();
+        ordersGrid.DataBind();
+    }
+}
