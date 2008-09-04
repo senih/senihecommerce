f59800f544093613458c2692b25bd365a9e3e3f6
vsenih <vsenih@f233bf74-7c54-0410-ac8d-d1e801669cec>
2008-09-04 14:12:28 +0000

    04.09.2008 Merchant calculation API implemented

--- trunk/OrdersProcessing/test.aspx.cs
+++ trunk/OrdersProcessing/test.aspx.cs
@@ -10,11 +10,12 @@ using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 using System.Xml.Linq;
+using Orders;
 
 public partial class OrdersProcessing_test : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        
     }
 }
